7b14650cc10ee495cf247f9c83caf2e87e6dcdce
Avchinnikova Alina <avchinnikova1012@gmail.com>
2023-05-17 21:07:45 +0300

    add class Wheel

---
+++ src/main/java/by/itacademy/alina/avchinnikova/javabasics/oop/Wheel.java
@@ -0,0 +1,9 @@
+package by.itacademy.alina.avchinnikova.javabasics.oop;
+
+public class Wheel {
+    String typeSeason;
+    int diameter;
+    String brand;
+    int yearOfIssue;
+    int price;
+}
